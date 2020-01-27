ccc0cfcc3e442db6886252c8af977260a31741b8
saszer <43649755+saszer@users.noreply.github.com>
2020-01-27 22:24:17 +1100

    unity proj

---
+++ Simple Bubble Burst acc to force/Assets/Scripts/dieandplayparticles.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class dieandplayparticles : MonoBehaviour
+{   
+
+    private Vector3 previousPos;
+    public float burstsenstivity;
+
+ 
+    void Die()
+    {
+
+        SkinnedMeshRenderer _renderer = this.GetComponent<SkinnedMeshRenderer>();
+        _renderer.enabled = false;
+        ParticleSystem _particleEmitter = this.GetComponentInChildren<ParticleSystem>();
+        _particleEmitter.Play();
+
+
+        //GetComponent("ParticleRenderer").enabled = true;
+        //Can destroy the ganeobject here after our burst animation is complete >>>
+        //yield WaitForSeconds(.5);
+        //Destroy(gameObject);
+    }
+
+
+    void OnCollisionEnter(Collision col)
+    {
+      if (col.relativeVelocity.magnitude > burstsenstivity)
+          Die();
+    }
+
+    /*
+     void FixedUpdate()
+    {
+
+        if(Vector3.Distance(this.transform.position , previousPos)>2f)
+            Die();
+        previousPos = this.transform.position;
+
+    }
+    */
+
+
+
+}
